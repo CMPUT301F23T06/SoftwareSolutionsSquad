4ff66ae225804d83a5a8d3dcb72ff36b5a26fdfd
Bhagya <bhagya02.p@gmail.com>
2023-11-03 00:36:20 -0600

    running app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
