d75194c7dbab8f32680ea06cb29007ac3ec92e6e
Bhagya <bhagya02.p@gmail.com>
2023-11-08 12:02:01 -0700

    updates to make it faster

--- .github/start_emu_headless.sh
+++ .github/start_emu_headless.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Colored output
 BL='\033[0;34m'
 G='\033[0;32m'
 RED='\033[0;31m'
 YE='\033[1;33m'
 NC='\033[0m' # No Color
 
-emulator_name=${EMULATOR_NAME}
+# Emulator name variable
+emulator_name=${EMULATOR_NAME:-"emulator"}
 
+# Function to check hardware acceleration
 function check_hardware_acceleration() {
     if [[ "$HW_ACCEL_OVERRIDE" != "" ]]; then
         hw_accel_flag="$HW_ACCEL_OVERRIDE"
@@ -20,85 +23,86 @@ function check_hardware_acceleration() {
             HW_ACCEL_SUPPORT=$(grep -E -c '(vmx|svm)' /proc/cpuinfo)
         fi
 
-        if [[ $HW_ACCEL_SUPPORT == 0 ]]; then
-            hw_accel_flag="-accel off"
-        else
-            hw_accel_flag="-accel on"
-        fi
+        hw_accel_flag=$HW_ACCEL_SUPPORT == 0 ? "-accel off" : "-accel on"
     fi
 
     echo "$hw_accel_flag"
 }
 
-hw_accel_flag=$(check_hardware_acceleration)
+# Function to launch the emulator
+function launch_emulator() {
+    adb devices | grep emulator | cut -f1 | xargs -I {} adb -s "{}" emu kill
+    hw_accel_flag=$(check_hardware_acceleration)
+    options="@${emulator_name} -no-window -no-snapshot -screen no-touch -noaudio -memory 2048 -no-boot-anim ${hw_accel_flag} -camera-back none"
 
-function launch_emulator () {
-  adb devices | grep emulator | cut -f1 | xargs -I {} adb -s "{}" emu kill
-  options="@${emulator_name} -no-window -no-snapshot -screen no-touch -noaudio -memory 2048 -no-boot-anim ${hw_accel_flag} -camera-back none"
-  if [[ "$OSTYPE" == *linux* ]]; then
-    echo "${OSTYPE}: emulator ${options} -gpu off"
-    nohup emulator $options -gpu off &
-  fi
-  if [[ "$OSTYPE" == *darwin* ]] || [[ "$OSTYPE" == *macos* ]]; then
-    echo "im here"
-    echo "${OSTYPE}: emulator ${options} -gpu swiftshader_indirect"
-    nohup emulator $options -gpu swiftshader_indirect &
-  fi
+    if [[ "$OSTYPE" == *linux* ]]; then
+        echo "${OSTYPE}: emulator ${options} -gpu off"
+        emulator $options -gpu off &
+    elif [[ "$OSTYPE" == *darwin* ]] || [[ "$OSTYPE" == *macos* ]]; then
+        echo "${OSTYPE}: emulator ${options} -gpu swiftshader_indirect"
+        emulator $options -gpu swiftshader_indirect &
+    else
+        echo "Unsupported OS"
+        exit 1
+    fi
 
-  if [ $? -ne 0 ]; then
-    echo "Error launching emulator"
-    return 1
-  fi
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}Error launching emulator${NC}"
+        exit 1
+    fi
 }
 
-function check_emulator_status () {
-  printf "${G}==> ${BL}Checking emulator booting up status 🧐${NC}\n"
-  start_time=$(date +%s)
-  spinner=( "⠹" "⠺" "⠼" "⠶" "⠦" "⠧" "⠇" "⠏" )
-  i=0
-  # Get the timeout value from the environment variable or use the default value of 300 seconds (5 minutes)
-  timeout=${EMULATOR_TIMEOUT:-400}
+# Function to check the emulator status
+function check_emulator_status() {
+    printf "${G}==> ${BL}Checking emulator booting up status 🧐${NC}\n"
+    start_time=$(date +%s)
+    timeout=${EMULATOR_TIMEOUT:-500} # Default timeout of 5 minutes
+    while true; do
+        result=$(adb shell getprop sys.boot_completed 2>&1)
+        if [[ "$result" == "1" ]]; then
+            printf "${G}==> \u2713 Emulator is ready${NC}\n"
+            adb devices -l
+            adb shell input keyevent 82
+            break
+        else
+            printf "${RED}==> Emulator is not ready yet. Status: $result ${NC}\n"
+            current_time=$(date +%s)
+            elapsed_time=$((current_time - start_time))
+            if [ $elapsed_time -ge $timeout ]; then
+                printf "${RED}==> Timeout after ${timeout} seconds${NC}\n"
+                exit 1
+            fi
+            sleep 5
+        fi
+    done
+}
 
-  while true; do
-    result=$(adb shell getprop sys.boot_completed 2>&1)
-
-    if [ "$result" == "1" ]; then
-      printf "\e[K${G}==> \u2713 Emulator is ready : '$result'           ${NC}\n"
-      adb devices -l
-      adb shell input keyevent 82
-      break
-    elif [ "$result" == "" ]; then
-      printf "${YE}==> Emulator is partially Booted! 😕 ${spinner[$i]} ${NC}\r"
-    else
-      printf "${RED}==> $result, please wait ${spinner[$i]} ${NC}\r"
-      i=$(( (i+1) % 8 ))
-    fi
-
-    current_time=$(date +%s)
-    elapsed_time=$((current_time - start_time))
-    if [ $elapsed_time -gt $timeout ]; then
-      printf "${RED}==> Timeout after ${timeout} seconds elapsed 🕛.. ${NC}\n"
-      break
-    fi
-    sleep 4
-  done
-};
-
+# Function to disable animations on the emulator for faster testing
 function disable_animation() {
-  adb shell "settings put global window_animation_scale 0.0"
-  adb shell "settings put global transition_animation_scale 0.0"
-  adb shell "settings put global animator_duration_scale 0.0"
-};
+    adb shell "settings put global window_animation_scale 0.0"
+    adb shell "settings put global transition_animation_scale 0.0"
+    adb shell "settings put global animator_duration_scale 0.0"
+}
 
+# Function to handle hidden API policies (necessary for some Android versions)
 function hidden_policy() {
-  adb shell "settings put global hidden_api_policy_pre_p_apps 1;settings put global hidden_api_policy_p_apps 1;settings put global hidden_api_policy 1"
-};
+    adb shell "settings put global hidden_api_policy_pre_p_apps 1"
+    adb shell "settings put global hidden_api_policy_p_apps 1"
+    adb shell "settings put global hidden_api_policy 1"
+}
 
-launch_emulator
-sleep 2
-check_emulator_status
-sleep 1
-disable_animation
-sleep 1
-hidden_policy
-sleep 1
+# Main logic to handle prelaunch and postlaunch
+case "$1" in
+    prelaunch)
+        launch_emulator
+        check_emulator_status
+        ;;
+    postlaunch)
+        disable_animation
+        hidden_policy
+        ;;
+    *)
+        echo "Invalid or no argument provided. Please use 'prelaunch' or 'postlaunch'."
+        exit 1
+        ;;
+esac
