e5f4300ac6cafb02867c72f69499e66a50769fd7
Pratham Sitoula <psitoula2003@gmail.com>
2023-11-29 12:13:36 -0700

    Completed Profile Page

--- app/src/main/java/com/example/softwaresolutionssquad/views/ProfileFragment.java
+++ app/src/main/java/com/example/softwaresolutionssquad/views/ProfileFragment.java
@@ -11,6 +11,12 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.softwaresolutionssquad.R;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class ProfileFragment extends Fragment {
 
@@ -21,9 +27,15 @@ public class ProfileFragment extends Fragment {
 
     private LinearLayout logoutBar;
 
-    private int tagCount = 0;
+    private CollectionReference itemsRef;
 
-    private int itemCount = 0;
+    private CollectionReference tagsRef;
+
+    private int tagCount;
+
+    private int itemCount;
+
+    private String userName;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -35,8 +47,14 @@ public class ProfileFragment extends Fragment {
         itemCountTextView = view.findViewById(R.id.itemAmount);
         tagCountTextView = view.findViewById(R.id.tagAmount);
         logoutBar = view.findViewById(R.id.logoutBar);
+        tagsRef = ((MainActivity) getActivity()).getDb().collection("Tags");
+        itemsRef = ((MainActivity)  getActivity()).getDb().collection("Item");
         MyApp myApp = (MyApp) requireActivity().getApplication();
         UserViewModel userViewModel = myApp.getUserViewModel();
+        userName = userViewModel.getUsername();
+        Log.d("user", userName);
+        getTagCount();
+        getItemCount();
 
         logoutBar.setOnClickListener(v -> {
             userViewModel.setUsername("");
@@ -44,5 +62,38 @@ public class ProfileFragment extends Fragment {
             startActivity(logoutIntent);
         });
         return view;
+    }
+
+    private void getItemCount() {
+        itemsRef.get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                itemCount = 0;
+                for (QueryDocumentSnapshot _ : task.getResult()) {
+                    itemCount += 1;
+                }
+                itemCountTextView.setText(String.valueOf(itemCount));
+
+            } else {
+                // Handle the error
+                Log.e("Item get Profile Fragment", "Error fetching tags from Firestore", task.getException());
+            }
+        });
+
+    }
+
+    private void getTagCount() {
+        tagsRef.whereEqualTo("user", userName).get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                tagCount = 0;
+                for (QueryDocumentSnapshot _ : task.getResult()) {
+                    tagCount += 1;
+                }
+                tagCountTextView.setText(String.valueOf(tagCount));
+
+            } else {
+                // Handle the error
+                Log.e("Tag get Profile Fragment", "Error fetching tags from Firestore", task.getException());
+            }
+        });
     }
 }
