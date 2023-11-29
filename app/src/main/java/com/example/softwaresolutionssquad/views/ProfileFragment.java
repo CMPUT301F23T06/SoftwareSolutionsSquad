842d1d8fea86a0dbfc16d5bc9233f0fad1e22965
Pratham Sitoula <psitoula2003@gmail.com>
2023-11-29 11:44:00 -0700

    Implemented Logout functionality

--- app/src/main/java/com/example/softwaresolutionssquad/views/ProfileFragment.java
+++ app/src/main/java/com/example/softwaresolutionssquad/views/ProfileFragment.java
@@ -1,4 +1,5 @@
 package com.example.softwaresolutionssquad.views;// Dummy fragment for the "Profile" page
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 
@@ -6,10 +7,23 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.softwaresolutionssquad.R;
 
 public class ProfileFragment extends Fragment {
+
+
+    private TextView itemCountTextView;
+
+    private TextView tagCountTextView;
+
+    private LinearLayout logoutBar;
+
+    private int tagCount = 0;
+
+    private int itemCount = 0;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -17,8 +31,18 @@ public class ProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.d("Profile", "hello");
-        // You can create a simple layout for the dummy fragment or leave it empty.
-        return inflater.inflate(R.layout.fragment_profile_layout, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile_layout, container, false);
+        itemCountTextView = view.findViewById(R.id.itemAmount);
+        tagCountTextView = view.findViewById(R.id.tagAmount);
+        logoutBar = view.findViewById(R.id.logoutBar);
+        MyApp myApp = (MyApp) requireActivity().getApplication();
+        UserViewModel userViewModel = myApp.getUserViewModel();
+
+        logoutBar.setOnClickListener(v -> {
+            userViewModel.setUsername("");
+            Intent logoutIntent = new Intent(requireContext(), LoginActivity.class);
+            startActivity(logoutIntent);
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/softwaresolutionssquad/views/UserViewModel.java
+++ app/src/main/java/com/example/softwaresolutionssquad/views/UserViewModel.java
@@ -7,21 +7,16 @@ import androidx.lifecycle.ViewModel;
 
 public class UserViewModel extends ViewModel {
     private String username = "";
-    private String name = "";
+
 
     public String getUsername() {
         return username;
     }
 
-    public String getName() {
-        return name;
-    }
+
 
     public void setUsername(String user) {
         this.username = user;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
